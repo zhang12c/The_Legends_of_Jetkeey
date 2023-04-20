1bd310d5745689bd21a8ade51792c334399e9204
jetkeey <jetkeey0317@gmail.com>
2023-04-21 01:50:59 +0800

    开始写pig敌人的动画

--- Assets/Scripts/Entity/Enemy/BaseEnemy.cs
+++ Assets/Scripts/Entity/Enemy/BaseEnemy.cs
@@ -1,8 +1,44 @@
+using System;
 using UnityEngine;
 namespace Entity.Enemy
 {
     public class BaseEnemy : MonoBehaviour
     {
+        // 巡逻的速度
+        public float normalSpeed;
+        // 追杀的速度
+        public float followSpeed;
+        // 现在的速度保留一下
+        public float currentSpeed;
+
+        protected Rigidbody2D Rb;
+        protected Animator Animator;
         
+        public Vector3 faceDir;
+
+        private void Awake()
+        {
+            Rb = GetComponent<Rigidbody2D>();
+            Animator = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            currentSpeed = normalSpeed;
+        }
+        private void Update()
+        {
+            faceDir = new Vector3(-transform.localScale.x, 0, 0);
+        }
+
+        private void FixedUpdate()
+        {
+            Movement();
+        }
+
+        protected virtual void Movement()
+        {
+            Rb.velocity = new Vector2(currentSpeed * faceDir.x , Rb.velocity.y);
+        }
     }
 }
--- Assets/Scripts/Entity/Enemy/Pig.cs
+++ Assets/Scripts/Entity/Enemy/Pig.cs
@@ -1,7 +1,15 @@
+using UnityEngine;
 namespace Entity.Enemy
 {
     public class Pig : BaseEnemy
     {
-        
+        private static readonly int Walk = Animator.StringToHash("walk");
+        private static readonly int Run = Animator.StringToHash("run");
+        protected override void Movement()
+        {
+            base.Movement();
+            Animator.SetBool(Walk,true);
+            //Animator.SetBool(Run,true);
+        }
     }
 }
