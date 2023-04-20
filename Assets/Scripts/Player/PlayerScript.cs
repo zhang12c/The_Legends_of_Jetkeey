f6c08ffccf98387f9c20b1c7ffdeb5661a106e6d
jetkeey <jetkeey0317@gmail.com>
2023-04-20 12:43:04 +0800

    跳起来

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -5,25 +5,32 @@ namespace Player
 {
     public class PlayerScript : MonoBehaviour
     {
-        private float _inputX;
-        private float _inputY;
-        private bool _turnLeft;
-        private bool _isMoving;
-
-        private Rigidbody2D _rigidbody2D;
-        private Animator _animator;
-
-        // xy 合成的一个向量，人物朝向
-        private Vector2 _movementInput;
-        public float moveSpeed;
-        
         
         private static readonly int IsMoving = Animator.StringToHash("isMoving");
         private static readonly int InputX = Animator.StringToHash("inputX");
         private static readonly int InputY = Animator.StringToHash("inputY");
         private static readonly int TurnLeft = Animator.StringToHash("turnLeft");
         private static readonly int Atk = Animator.StringToHash("atk");
+        
+        private Rigidbody2D _rigidbody2D;
+        private Animator _animator;
+        
+        private float _inputX;
+        private float _inputY;
+        private bool _turnLeft;
+        private bool _isMoving;
+        
         private bool _inputDisable;
+        public float moveSpeed;
+        public float jumpForce;
+
+        public Transform groundCheck;
+        private bool _isGround;
+        public LayerMask whatIsGround;
+        public float groundCheckRadius;
+        private bool _canJump;
+        public int amountOfJumps = 1;
+        private int _amountOfJumpsLeft;
 
         private void Awake()
         {
@@ -41,15 +48,29 @@ namespace Player
                 // 不可输入的时候就不要跑步了
                 _isMoving = false;
             }
+            CheckIfCanJump();
             SwitchAnimator();
         }
-        
+        private void CheckIfCanJump()
+        {
+            if (_isGround )
+            {
+                _canJump = true;
+            }
+            else
+            {
+                _canJump = false;
+            }
+        }
+
         private void FixedUpdate()
         {
             if (!_inputDisable)
             {
                 Movement();
             }
+
+            CheckSurroundings();
         }
 
         private void PlayerInput()
@@ -61,12 +82,14 @@ namespace Player
             if (!Input.GetKey(KeyCode.LeftShift))
                 _inputX *= 0.5f;
             
-            _movementInput = new Vector2(_inputX, 0);
-            _isMoving = _movementInput != Vector2.zero;
-            
             if (_inputX != 0)
             {
+                _isMoving = true;
                 _turnLeft = _inputX < 0;
+            }
+            else
+            {
+                _isMoving = false;
             }
 
             if (Input.GetKeyDown(KeyCode.J))
@@ -74,11 +97,23 @@ namespace Player
                 _animator.SetTrigger(Atk);
                 _inputDisable = false;
             }
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                Jump();
+            }
             
+        }
+        private void Jump()
+        {
+            if (_canJump)
+            {
+                _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x,jumpForce);
+            }
         }
         private void Movement()
         {
-            _rigidbody2D.velocity = _movementInput * moveSpeed;
+            _rigidbody2D.velocity = new Vector2(_inputX * moveSpeed,_rigidbody2D.velocity.y);
         }
         
         void SwitchAnimator()
@@ -91,6 +126,11 @@ namespace Player
             _animator.SetBool(IsMoving,_isMoving);
             _animator.SetBool(TurnLeft,_turnLeft);
             
+        }
+
+        private void CheckSurroundings()
+        {
+            _isGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
         }
     }
 }
